1c90d2e5879c7a72a64ae151f1224813b2b5f936
Shariq Hashme <shashme@umd.edu>
2014-10-25 19:48:23 -0400

    fixed syntax errors in script.py

--- start.py
+++ start.py
@@ -20,10 +20,9 @@ import pulse
 
 def __main__():
     q = Queue(10000)	#tinker with the size
-    producer = Thread(Target=pulse.stream_thread, (args=q,))
-    consumer = Thread(Target=pulse.remove_thread, (args=q,))
-    #analyzer = Thread(Target=pulse.analysis_thread, (args=q,))
-
+    producer = Thread(target=pulse.stream_thread, args=(q,))
+    consumer = Thread(target=pulse.remove_thread, args=(q,))
+    #analyzer = Thread(target=pulse.analysis_thread, (args=q,))
     producer.start()
     consumer.start()
     #analyzer.start()
