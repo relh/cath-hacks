6bdfce8c219764638dca8cb7848de4ee585c42ac
Anthony Castrio <anthonycastrio@gmail.com>
2014-10-25 18:48:43 -0400

    created pulse_helper.py

--- pulse.py
+++ pulse.py
@@ -1,24 +1,24 @@
-#from analyze import find_event?
+from pulse_helper import analyze, trigger
 
 #---needs to be tested!
 def tidapiobj_to_html(tweetid, apiobject):
- return json.loads(apiobject.get_oembed(id=tweetid))['html']
+    return json.loads(apiobject.get_oembed(id=tweetid))['html']
 
 #---streams tweets into queue
 def stream_thread(queue):
- l = StdOutListener()
- auth = OAuthHandler(consumer_key, consumer_secret)
- auth.set_access_token(access_token, access_token_secret)
- api_object = API(auth)
- stream = Stream(auth, l)
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api_object = API(auth)
+    stream = Stream(auth, l)
 
 #---removes tweets from the bottom of the queue
 def remove_thread(queue):
- #remove old tweets
- return
+    #remove old tweets
+    return
 
 #---analyzes queue for news events
 def analysis_thread(queue):
- #analyze current queue
- return
+    #analyze current queue
+    return
 
---
+++ pulse_helper.py
@@ -0,0 +1,8 @@
+
+#---Analyzes tweets for news events
+def analyze(tweets):
+    print ""
+
+#---Send information to database
+def trigger(event):
+    print ""
