a73160f4133c86a66e8155704ef5b19829806671
Tom Cornelius <t.cornelius11@gmail.com>
2014-10-26 06:57:37 -0700

    analyze.py

--- analyze.py
+++ analyze.py
@@ -4,6 +4,7 @@ import requests
 import json
 import re
 import random
+from data_analysis import prune
 
 analyze_session = requests.session()
 
@@ -29,24 +30,63 @@ def clean(message):
 def analyze(tweets):
     event = None
     
+    keywords = [u'rain',u'rainy',u'storm',u'stormy',u'sunny',u'snow',u'snowy',u'cloudy',u'clear',u'windy',u'wind',u'bright']
+    temperature_words = [u'cold',u'freezing',u'frigid',u'chilly',u'mild',u'warm',u'hot',u'scorching',u'scorcher',u'heat']
+    all_tweets = copy.copy(tweets)      #copy of tweets
+    word_freq = {}      #frequency list
     for tweet in tweets:
-        print clean(tweet['text'])
-        if 'obama' in tweet['text'] or random.random() > 0.8:
-            if not event:event = {}
-            event[str(tweet['id'])] = {'Lat':tweet['latlong'][0], 'Long':tweet['latlong'][1],\
-                                       'Timestamp':tweet['timestamp'],'Data':tweet['id']}
+        #print clean(tweet['text'])
+        if tweet['text'][:2].lower() == 'rt':continue
+        #---Your code goes here! Try stuff to find events!
+        #^- frequency analysis?
+        txt = clean(tweet['text'])
+        #txt = tweet['text']
+        words = txt.split(' ')  #wordlist
+        #print txt
+        #print words
+        for w in words:
+            if not w.lower() in word_freq:
+                word_freq[w.lower()] = 1
+            else:
+                word_freq[w.lower()] += 1
+    freq = prune(word_freq)
+    freq2 = sorted(freq, key=freq.get)
+    freq = freq2[len(freq2)-30:len(freq2)]  #top 30 current words
+    #print freq
+    i = 0
+    for f in freq:
+        if f in keywords:       #tweak
+            #print "keyword found: "+f
+            #print len(all_tweets)
+            for t in all_tweets:
+                if t['text'][:2].lower() == 'rt':continue
+                if f in clean(t['text']).split(' '):
+                    if not event:
+                        event = {}
+                        event['tweets'] = []
+                    event['tweets'].append({'latlong':t['latlong'],'timestamp':t['timestamp'],'id':t['id']})
+                    event['keywords'] = [f]
+                    for temp in temperature_words:
+                        if temp in clean(t['text']).split(' '):
+                            event['keywords'].append(temp)
+
+            break
+        i += 1
+
+
     
     # SET EVENT DESCRIPTION IN VAR DESCRIPTION
-    description = 'beautiful weather'
+    description = 'weather'
     # EVENT MUST CONTAIN TWEETS WITH LAT,LONG,TIMESTAMP,DATA
     if event:
         print 'event detected!'
+        print event
         tweets = [event[x] for x in event if isinstance(event[x], dict)]
-        avg_lat = sum(map(lambda x:x['Lat'], tweets))/float(len(tweets))
-        avg_long = sum(map(lambda x:x['Long'], tweets))/float(len(tweets))
-        avg_time = sum(map(lambda x:x['Timestamp'], tweets))/float(len(tweets))
-        event['Lat'] = avg_lat
-        event['Long'] = avg_long
-        event['Keywords'] = description
-        event['Timestamp'] = avg_time
-        analyze_session.post(FIREBASE_EVENTS_ENDPOINT, data=json.dumps(event))
+        #avg_lat = sum(map(lambda x:x['Lat'], tweets))/float(len(tweets))
+        #avg_long = sum(map(lambda x:x['Long'], tweets))/float(len(tweets))
+        #avg_time = sum(map(lambda x:x['Timestamp'], tweets))/float(len(tweets))
+        #event['Lat'] = avg_lat
+        #event['Long'] = avg_long
+        #event['Keywords'] = description
+        #event['Timestamp'] = avg_time
+        #analyze_session.post(FIREBASE_EVENTS_ENDPOINT, data=json.dumps(event))
