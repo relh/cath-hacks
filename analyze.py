4cd313d6aaf864577ded287d754d5c61ac824d80
Shariq Hashme <shariqhashme@gmail.com>
2014-10-26 08:46:16 -0400

    messing with data and fixed clean function

--- analyze.py
+++ analyze.py
@@ -10,20 +10,20 @@ analyze_session = requests.session()
 #---Gets rid of extra punctuation, not latin characters, and urls
 def clean(message):
     out = ''
-    words = message.replace('\n',' ')
-    bad_words = ['a','the','he','she']
+    words = message.replace('\n',' ').split()
+    bad_words = [u'hour',u'are',u'here',u'much',u'things',u'than',u'there',u'much',u'from',u'still',u'being',u'into',u'out',u'every',u'they',u'now',u'were',u'very',u'after',u'would',u'could',u'can',u'can',u'will',u'doe',u'thats',u'why',u'take',u'cant',u'well',u'look',u'know',u'all',u'ur',u'what',u'who',u'where',u'or',u'do',u'got',u'when',u'no',u'u',u'im',u'dont',u'how',u'if',u'as',u'nd',u'up',u'by',u'what',u'about',u'was',u'',u'its',u'in',u'too',u'a',u'an',u'i',u'he',u'me',u'she',u'we',u'the',u'to',u'are',u'you',u'him',u'her',u'my',u'and',u'is',u'of',u'to',u'rt',u'for',u'on',u'it',u'that',u'this',u'be',u'just',u'like',u'lol',u'rofl',u'lmao',u'your',u'have',u'but',u'you',u'not',u'get',u'so',u'at',u'with']
     for word in words:
-        if word in bad_words:
+        if word.lower() in bad_words:
             continue
         if word[0] is '@' or word[:2] is '.@':
             continue
-        if word[:5] == 'http:':
+        if word.lower()[:5] == 'http:':
             continue
         if word == 'RT':
             return ''
         out += ''.join([c for c in word if c.lower() in 'abcdefghijklmnopqrstuvwxyz0123456789 '])
         out += ' '
-    return out
+    return out.lower().strip()
 
 #---Analyzes tweets for news events
 def analyze(tweets):
